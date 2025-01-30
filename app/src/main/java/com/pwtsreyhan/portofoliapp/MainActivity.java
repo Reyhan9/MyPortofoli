ddd206c7c51c16505fde432c85fef228c3a4a2bb
Reyhan9 <namestaknamestak@gmail.com>
2025-01-30 13:20:13 +0700

    feat: Implement profile bottom sheet
    This commit introduces a new profile bottom sheet, accessible via a new button in the main activity.
    
    -   Added `ProfileSheet.java`: This file defines a new `BottomSheetDialogFragment` subclass for displaying profile information.
    -   Added `fragment_profile_sheet.xml`: This layout file defines the structure and content of the profile sheet, including an image and a descriptive text.
    -   Modified `MainActivity.java`: Added an `OnClickListener` to the profile image button. When clicked, a `ProfileSheet` instance will be displayed as a bottom sheet.
    -   Modified `activity_main.xml`: Added an image button for the profile.
    -   Added `background_sheet.xml`: Defines the background for the profile sheet with rounded top corners.
    -   Modified `strings.xml`: Added new string resources for profile description and the profile sheet fragment.
    -   Modified `themes.xml`: Added navigation bar color customization.
    - Modified `themes.xml` in night mode to custom navigation bar color.
    -Modified `deploymentTargetSelector.xml`: Added a new deployment target selection.

--- app/src/main/java/com/pwtsreyhan/portofoliapp/MainActivity.java
+++ app/src/main/java/com/pwtsreyhan/portofoliapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pwtsreyhan.portofoliapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnProfile, btnHardskill, btnProject, btnContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnProfile = findViewById(R.id.profile);
+        btnHardskill = findViewById(R.id.hardskill);
+        btnProject = findViewById(R.id.project);
+        btnContact = findViewById(R.id.contact);
+
+        btnProfile.setOnClickListener(view -> {
+            ProfileSheet ProfileSheet = new ProfileSheet();
+            ProfileSheet.show(getSupportFragmentManager(), ProfileSheet.getTag());
+        });
+
     }
 }
